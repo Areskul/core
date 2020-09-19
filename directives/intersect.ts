6943545f04b1314e947a47427c5c58d7bc33187c
Areskul <the.only.areskul@gmail.com>
2020-09-19 17:08:32 +0200

    new intersect directive typescript

---
+++ directives/intersect.ts
@@ -0,0 +1,36 @@
+interface Config {
+  threshold: number;
+  root: null;
+  rootMargin: string;
+}
+
+const defaultConfig: Config = {
+  threshold: 0,
+  root: null,
+  rootMargin: "0px",
+};
+
+function except(object: any, keys: any) {
+  return Object.keys(object).reduce((reduced: any, key: any) => {
+    if (!keys.includes(key)) {
+      reduced[key] = object[key];
+    }
+    return reduced;
+  }, {});
+}
+
+function parseIntersectValue(value: any) {
+  return typeof value === "function"
+    ? Object.assign({}, defaultConfig, { callback: value })
+    : Object.assign({}, defaultConfig, value);
+}
+
+export const intersect = function inserted(el: HTMLElement, { value }: any) {
+  const config = parseIntersectValue(value);
+
+  const observer = new IntersectionObserver(([entry]) => {
+    config.callback(entry);
+  }, except(config, ["callback"]));
+
+  observer.observe(el);
+};
