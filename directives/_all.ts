495dd171e55ffd13841e6c58a8bd5ecdad08e4c9
Areskul <the.only.areskul@gmail.com>
2020-09-21 10:44:38 +0200

    [ scss ] unselectrrable text and unsavable pictures

--- directives/_all.ts
+++
@@ -1,2 +0,0 @@
-import { scroll } from "./scroll";
-export { scroll };
---
+++ directives/index.ts
@@ -0,0 +1,6 @@
+import { scroll } from "./scroll";
+export { scroll };
+import { intersect } from "./intersect";
+export { intersect };
+// import { outsideClick } from "./outsideClick";
+// export { outsideClick };
